4bc74a42a19f84fc1f9f8cf8b08c538ccc49faea
hfwin <475947645@qq.com>
2021-12-24 16:19:51 +0800

    [docs] 项目添加version信息

--- aiutils/__init__.py
+++ aiutils/__init__.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
-"""
-@time: 2021/10/20 8:55
-@file: __init__.py.py
+# @time: 2021/12/24 16:14
+# @Author：lhf
+
+
+CHANGE_LOG = """
+ 1.1.0
+升级code模块，合约转换的规则
+
+1.0.0
+迁移大部分功能
 
 """
+
+__version__ = CHANGE_LOG.split()[0]
